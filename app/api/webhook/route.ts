29f112c26405e5e972579cd65de73e2d677f961d
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-01 22:04:26 -0300

    add route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,7 @@
 import {Webhook} from "svix";
 import {headers} from "next/headers";
 import {WebhookEvent} from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+}
