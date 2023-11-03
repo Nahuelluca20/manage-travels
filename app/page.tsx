52c2a6b788c8b7e552cb752806da383f32f0db52
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-03 20:10:08 -0300

    proof queries in de server and in the client

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import {serverClient} from "./_trpc/server-client";
 import SignOut from "./components/signout-button";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await serverClient.getTodos();
+
+  console.log(todos);
+
   return (
     <main>
       <h1 className="text-white">hola</h1>
+      {todos?.map((todo, i) => <h1 key={i}>{todo}</h1>)}
       <SignOut />
     </main>
   );
