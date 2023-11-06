e5371ab4ae15772bfca687da58e0b89da2b81e9d
Nahuel Luca <nahuelluca20@gmail.com>
2023-11-06 11:05:36 -0300

    add: search bar

---
+++ components/cards/travel-card.tsx
@@ -0,0 +1,22 @@
+import {serverClient} from "@/app/_trpc/server-client";
+
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "../ui/card";
+
+export default async function TravelCard({travelId}: {travelId: string}) {
+  const travelInfo = travelId && (await serverClient.getTravelInfoById(travelId));
+
+  if (!travelInfo) {
+    throw travelInfo;
+  }
+
+  return (
+    <Card className="w-full max-w-[300px]">
+      <CardHeader>
+        <CardTitle>{travelInfo[0].title}</CardTitle>
+        <CardDescription>
+          {travelInfo[0].city}, {travelInfo[0].country}
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
